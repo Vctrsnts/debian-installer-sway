4709dc7819af4645cf8e5832e379774b92f99407
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-08-25 16:55:25 +0200

    Add files via upload

--- custom-configs/sway/config.d/background-rotator.sh
+++ custom-configs/sway/config.d/background-rotator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  IMAGE=$(find "$HOME/.config/background" -type f | shuf -n 1)
+  IMAGE=$(find "$HOME/.config/backgrounds" -type f | shuf -n 1)
   pkill swaybg
   swaybg -i "$IMAGE" -m fill &
   sleep 3600
