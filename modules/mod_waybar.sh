8c7bde0b7c75f9cf03a99f243a5c4198e0c8b6d0
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-08-26 18:55:21 +0200

    Delete modules/mod_waybar.sh

--- modules/mod_waybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Módulo: Waybar - Instalación mínima (sin configuración)
-
-mod_waybar(){
-  [[ "${ENABLE_WAYBAR^^}" == "NO" ]] && { log_warn "🚫 Saltando Waybar."; return; }
-  apt_install waybar
-  log_success "🎉 Waybar instalado."
-}
