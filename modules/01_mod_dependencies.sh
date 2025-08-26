66bd724c0f48938f2b7f0e7b83a69607bfda66a9
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-08-26 23:49:29 +0200

    Add files via upload

--- modules/01_mod_dependencies.sh
+++ modules/01_mod_dependencies.sh
@@ -6,11 +6,11 @@ mod_dep(){
   [[ "${ENABLE_DEPENDENCIES^^}" == "NO" ]] && { log_warn "🚫 Desactivada la instalacio de dependencies."; return; }
 
   # === 2. Instalar dependencias de compilación ===
-  local pkgs=(
-    wget curl gpg
-  )
+  #local pkgs=(
+  #  wget curl gpg
+  #)
 
-  apt_install "${pkgs[@]}"
+  #apt_install "${pkgs[@]}"
 
   log_info "🎉 Ha finalitzat la instalacio de les dependencies."
 }
