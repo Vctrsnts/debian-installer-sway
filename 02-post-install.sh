b89c29df52a3b7072bc183114a57ea168bb2a3ce
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-08-29 13:40:27 +0200

    Add files via upload

--- 02-post-install.sh
+++ 02-post-install.sh
@@ -8,19 +8,9 @@ apt_install(){
 }
 
 # cd $HOME/debian-installer-sway/custom-configs/greetd
-# sudo cp * /etc/greetd/
-# sudo chmod go+r /etc/greetd
-
-  # GENERACIO DEL FITXERR SWAY-CONFIG PER AL FUNCIONAMENT DE GTKGREET + GREETD
-  sudo cat << EOF > "/etc/greetd/sway-config"
-exec "gtkgreet -l -s /etc/greetd/gtkgreet.css; swaymsg exit"
-
-bindsym Mod4+Shift+e exec swaynag \
-  -t warning \
-  -m 'Que vols fer?' \
-  -b 'Poweroff' 'systemctl poweroff' \
-  -b 'Reboot' 'systemctl reboot'
-EOF
+sudo cp $HOME/debian-installer-sway/custom-configs/greetd/* /etc/greetd/
+sudo chmod go+r /etc/greetd
+sudo systemctl enable greetd
 
 # instalacion de aplicaciones externas
 pkgs=(
