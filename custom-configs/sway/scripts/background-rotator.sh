b5d1d45a5587d3c2b92ffdcc31b02849f4fb9c84
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-08-29 10:10:40 +0200

    Add files via upload

--- custom-configs/sway/scripts/background-rotator.sh
+++ custom-configs/sway/scripts/background-rotator.sh
@@ -2,7 +2,8 @@
 
 while true; do
   IMAGE=$(find "$HOME/.config/backgrounds" -type f | shuf -n 1)
-  pkill swaybg
+  pkill -x swaybg   # Mata solo procesos swaybg
   swaybg -i "$IMAGE" -m fill &
-  sleep 3600
+  # sleep 3600 # 1 hora
+  sleep 300 # modificado a 5 minutos para hacer pruebas
 done
