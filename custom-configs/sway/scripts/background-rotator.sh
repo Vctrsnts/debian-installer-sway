fc9196b9a80a029e057548ee9fbb6093ed966d93
Vctrsnts <vctrsnts@gmail.com>
2025-09-01 22:54:09 +0200

    25090122:54 - Actualizacio fitxers

--- custom-configs/sway/scripts/background-rotator.sh
+++ custom-configs/sway/scripts/background-rotator.sh
@@ -4,6 +4,5 @@ while true; do
   IMAGE=$(find "$HOME/.config/backgrounds" -type f | shuf -n 1)
   pkill -x swaybg   # Mata solo procesos swaybg
   swaybg -i "$IMAGE" -m fill &
-  # sleep 3600 # 1 hora
-  sleep 300 # modificado a 5 minutos para hacer pruebas
+  sleep 3600 # 1 hora
 done
--- custom-configs/sway/scripts/wofi-menu.sh
+++ custom-configs/sway/scripts/wofi-menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chosen=$(echo -e " Apagar\n Reiniciar\n Cerrar sesión" | \
-  wofi --dmenu --prompt "Acción" --width 300 --height 200)
+  wofi --dmenu --prompt "¿Que quieres hacer?" --width 300 --height 200)
 
 case "$chosen" in
   " Apagar") systemctl poweroff ;;
